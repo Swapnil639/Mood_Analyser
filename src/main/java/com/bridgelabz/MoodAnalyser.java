0c77b725ca3eb257a3f07bd6a8df7a6ac8cf5c5f
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-10 12:28:27 +0530

    Refactor:To take mood message in Constructor

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,6 +1,15 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
+    String message;
+    public MoodAnalyser() {
+
+    }
+
+    public MoodAnalyser(String message) {
+        this.message=message;
+    }
+
     public String analyseMood(String message) {
         if (message.contains("I am in sad mood")){
             return "SAD";
@@ -9,4 +18,15 @@ public class MoodAnalyser {
         }
         return null;
     }
+
+    public String analyseMoodConstructor() {
+        if (message.contains("I am in sad mood")){
+            return "SAD";
+        } else if (message.contains("I am in happy mood")) {
+            return "HAPPY";
+        }
+        return null;
+    }
+
+
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -15,4 +15,15 @@ public class MoodAnalyserTest {
         String mood=moodAnalyser.analyseMood("I am in happy mood");
         Assertions.assertEquals("HAPPY",mood);
     }
+    @Test
+    public void givenTheMoodShouldReturnSadUsingConstructor(){
+        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in sad mood");
+        Assertions.assertEquals("SAD",moodAnalyser.analyseMoodConstructor());
+    }
+    @Test
+    public void givenTheMoodShouldReturnHappyUsingConstructor(){
+        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in happy mood");
+        Assertions.assertEquals("HAPPY",moodAnalyser.analyseMoodConstructor());
+    }
+
 }
